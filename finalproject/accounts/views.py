08045c4a3da4d6dc9e2c2b75b5f7c38027ab2303
ziboman98 <56830653+ziboman98@users.noreply.github.com>
2020-07-02 12:28:17 +0200

    fix bugs in gpa calc

--- finalproject/accounts/views.py
+++ finalproject/accounts/views.py
@@ -78,6 +78,7 @@ def graduation(request):
     """This method checks whether the student fulfills all the requirements for graduation"""
     student = get_object_or_404(Student, user=request.user)
     courses_complete = student.usercourses.all().filter(course_grade__gte=50)
+    programme_credits = 120
     arr = []
     for i in courses_complete:
         for j in Course.objects.all():
@@ -86,7 +87,6 @@ def graduation(request):
     total_credits = 0
     for i in arr:
         total_credits += i.credits
-
     programme_ = student.programme
     coursesInMyProgramme = Course.objects.filter(programme__plan_code=programme_.plan_code)
 
@@ -98,24 +98,18 @@ def graduation(request):
         f.add(i)
 
     # len on f should be equal to the len of courses_complete to graduate
-
-
-    print(programme_)
-    print(coursesInMyProgramme)
-
-    if len(f) < len(courses_complete) and total_credits > 120:
-        print("graduate!")
+    # print(programme_)
+    # print(coursesInMyProgramme)
+    message = False
+    if len(f) >= len(courses_complete) and total_credits > 120:
+        message = True
     else:
-        print("no")
-
-
-
+        pass
 
     # get all the courses programme requires
 
-    context = {'courses_complete': courses_complete, 'programme_credits': programme_credits}
+    context = {'message': message, 'programme_credits': programme_credits}
     return render(request, 'accounts/graduation.html', context)
-    # pass
 
 
 # @login_required(login_url='login')
@@ -127,72 +121,47 @@ def complete_courses(request):
         student = get_object_or_404(Student,user = request.user)
         courses_complete = student.usercourses.all().filter(course_grade__gte=50)
         courses = student.usercourses.all().filter(course_grade__gte=50).values('course_code')
-        """arr = []
-        for i in courses_complete:
-            for j in Course.objects.all():
-                if i.course_code == j:
-                    arr.append(j)
-        total_credits = 0
-        for i in arr:
-            total_credits+=i.credits
 
-        print(student)
-        print(total_credits)
-
-        programme_ = student.programme
-        coursesInMyProgramme = Course.objects.filter(programme__plan_code=programme_.plan_code)
-
-        f = set()
-
-        for i in coursesInMyProgramme:
-            f.add(i)
-        for i in courses_complete:
-            f.add(i)
-
-        # len on f should be equal to the len of courses_complete to graduate
-        if len(f) < len(courses_complete) and total_credits > 120:
-            print("graduate!")
-        else:
-            print("no")
-"""
-        """for i in courses:
-            credits =+ i.credits
-        print(credits)"""
         # courses_incomplete = student.usercourses.all().filter(course_grade__lt=50)
     except Exception:
         messages.error('no course complete')
     context = {'courses_complete': courses_complete}
     return render(request, "accounts/complete.html", context)
 
-
-
-
-
 # converting HTML to pdf
 # @login_required(login_url='login')
 # @allowed_users(allowed_roles=['student', 'admin'])
 def incomplete_courses(request):
-    # student = get_object_or_404(Student,user = request.user)
-    # programme = 
-    # student_programme = student.programme.all()
-    # programme_courses =  
-    # courses_complete = student.usercourses.all().filter(course_grade__null=True)
-    # print (student_programm)
-    # print("hg")
-    #return render(request, "accounts/incomplete.html", {'student':student_programm, 'courses_incomplete':courses_incomplete})
+    """This method displays the courses which a student has not passed wit grade of
+           50 and above or has never taken.
+       """
+    try:
+        student = get_object_or_404(Student, user=request.user)
+        # courses_complete = student.usercourses.all().filter(course_grade__gte=50)
+        courses_complete = student.usercourses.all().filter(course_grade__gte=50).values('course_code')
+        programme_ = student.programme
+        coursesInMyProgramme = Course.objects.filter(programme__plan_code=programme_.plan_code).values('course_code')
+        print(coursesInMyProgramme)
+        lst = []
+        for i in coursesInMyProgramme:
+            if i not in courses_complete:
+                lst.append(i)
 
-    pass
+
+    except Exception:
+        messages.error('No course incomplete')
+    context = {'courses_incomplete': lst}
+    return render(request, "accounts/incomplete.html", context)
+
 
 # converting HTML to pdf
 # @login_required(login_url='login')
 # @allowed_users(allowed_roles=['student', 'admin'])
-def generate_semester_pdf(request):
-    # return render(request, 'accounts/semester.html') 
-    pass
+
 
 
 def semester(request):
-
+    """This functions determines the amount of semesters left for a user"""
     student = get_object_or_404(Student, user=request.user)
     courses_complete = student.usercourses.all().filter(course_grade__gte=50)
     arr = []
@@ -204,8 +173,41 @@ def semester(request):
     for i in arr:
         total_credits += i.credits
     programme_ = student.programme
-    how_many_semester_left = int(programme_.overall_program_credits)/15
-    how_many_semester_left = math.floor(how_many_semester_left)
-    context = {'semester_left':how_many_semester_left,'programme':programme_}
-    return render()
+    whatsleft = (120 - int(programme_.overall_program_credits))/15
+    whatsleft = math.floor(whatsleft)
+    semestersDone = int(programme_.overall_program_credits)/15
+    how_many_semester_left = math.floor(semestersDone)
+    context = {'semester_left':how_many_semester_left, 'whatsleft':whatsleft}
+    return render(request, "accounts/semester.html", context)
 
+#  """arr = []
+#         for i in courses_complete:
+#             for j in Course.objects.all():
+#                 if i.course_code == j:
+#                     arr.append(j)
+#         total_credits = 0
+#         for i in arr:
+#             total_credits+=i.credits
+#
+#         print(student)
+#         print(total_credits)
+#
+#         programme_ = student.programme
+#         coursesInMyProgramme = Course.objects.filter(programme__plan_code=programme_.plan_code)
+#
+#         f = set()
+#
+#         for i in coursesInMyProgramme:
+#             f.add(i)
+#         for i in courses_complete:
+#             f.add(i)
+#
+#         # len on f should be equal to the len of courses_complete to graduate
+#         if len(f) < len(courses_complete) and total_credits > 120:
+#             print("graduate!")
+#         else:
+#             print("no")
+# """
+#         """for i in courses:
+#             credits =+ i.credits
+#         print(credits)"""
